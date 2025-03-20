d9ddbd4c12dcc31febfd98e2843bd313a2147e28
SvetlanaKobyakova <kobyakasv@mail.ru>
2025-03-20 21:48:10 +0300

    2003

--- BlogSite/urls.py
+++ BlogSite/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from blog.views import index, about
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('about/', about, name='about'),
+    path('', index, name='index'),
 ]
